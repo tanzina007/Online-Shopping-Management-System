1e7cba7e923a6ba1b1e83fc26b4d11f6f2e9cbfd
Tanzina Khan <20tanzina.khan@gmail.com>
2025-05-05 14:21:20 +0600

    Add files via upload

---
+++ src/loginandsignup/LoginAndSignup.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package loginandsignup;
+
+
+public class LoginAndSignup {
+
+    
+    public static void main(String[] args) {
+         Login Loginframe=new Login();
+         Loginframe.setVisible(true);
+         Loginframe.pack();
+         Loginframe.setLocationRelativeTo(null);
+    }
+    
+}
